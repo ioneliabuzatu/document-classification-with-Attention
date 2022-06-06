cd313e40b02407159fb5b2ed091a66c38ec8f94c
ioneliabuzatu <buzatuionelia@gmail.com>
2022-06-06 14:47:40 -0400

    config separate

---
+++ config.py
@@ -0,0 +1,18 @@
+import experiment_buddy
+
+
+lr = 1e-4
+which_optimizer = 'adam'
+attention = "dot"
+batch_size = 32
+hidden_size = 512
+document_length = -1
+epochs = 50
+weight_decay = 0.0
+
+
+experiment_buddy.register_defaults({'task': 'document classification with attention'})
+writer = experiment_buddy.deploy(
+    host="mila", disabled=False, wandb_kwargs={'project': "nlp"},
+    sweep_definition="sweep.yaml"
+)
